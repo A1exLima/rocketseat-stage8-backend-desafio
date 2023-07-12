892aac5c40db033671cf6d776855de64ea80871d
Alex Lima <alex.lima_dev@outlook.com>
2023-07-12 11:24:18 -0300

    Create route session

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ const express = require("express")
 const routes = express()
 
 const usersRoutes = require("../routes/usersRoutes.js")
-routes.use("/users", usersRoutes)
-
 const moviesRoutes = require("../routes/moviesRoutes.js")
-routes.use("/movies_notes", moviesRoutes)
-
 const tagsRoutes = require("../routes/tagsRoutes.js")
+const sessionsRoutes = require("../routes/sessionsRouter.js")
+
+routes.use("/users", usersRoutes)
+routes.use("/movies_notes", moviesRoutes)
 routes.use("/movies_tags", tagsRoutes)
+routes.use("/sessions", sessionsRoutes)
 
 module.exports = routes
